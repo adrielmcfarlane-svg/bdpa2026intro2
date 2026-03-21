047cd6343cba571b59fe40a5072f57ae8363c8ef
adrielmcfarlane-svg <adriel.mcfarlane@gmail.com>
2026-03-21 17:55:17 +0000

    a

---
+++ views/regrsterscipt.js
@@ -0,0 +1,21 @@
+const pwinput=document.querySelector("password");
+const pwstrength=document.getElementById("pwstrength");
+
+pwinput.addEventListener("input", updatePasswordStrength);
+    
+function updatePasswordStrength(e){
+    var pwlength=e.tarfet.value.length;
+    if (pwlength<=10){
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
